7a2e362b3de04de0568d24ddd22313b36d28f267
Ilyakiki <ilya765tolkiy@gmail.com>
2022-11-18 01:02:32 +0300

    Git pages

--- rekruto/urls.py
+++ rekruto/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('rekruto/templates/rekruto/',views.home),
-    path('rekruto/templates/rekruto/rekruto',views.rekruto)
+    path('',views.home),
+    path('rekruto',views.rekruto)
 ]
